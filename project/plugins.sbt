e709ec1e49c9a51714cbaf53fb839d76207b7f97
Gasper Kojek <gkojek@gradle.com>
2023-08-25 12:10:52 +0200

    Cleanup

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,4 +1,4 @@
 resolvers += Resolver.mavenLocal
 lazy val geVersion = "0.9"
 addSbtPlugin("com.gradle" % "sbt-gradle-enterprise" % geVersion)
-addSbtPlugin("com.gradle" % "common-custom-user-data-scala-plugin" % "0.1.0-SNAPSHOT")
+//addSbtPlugin("com.gradle" % "common-custom-user-data-scala-plugin" % "0.1.0-SNAPSHOT")
--- src/main/scala/com/gradle/CommonCustomUserDataScalaPlugin.scala
+++ src/main/scala/com/gradle/CommonCustomUserDataScalaPlugin.scala
@@ -8,30 +8,12 @@ import sbt._
 
 object CommonCustomUserDataScalaPlugin extends AutoPlugin {
 
-  object autoImport {
-    val helloGreeting = settingKey[String]("greeting")
-    val hello = taskKey[Unit]("say hello")
-  }
-
-  //    val autoImport = GradleEnterprisePlugin.autoImport
-
-  import autoImport._
-
   override def requires: Plugins = com.gradle.enterprise.sbt.GradleEnterprisePlugin
 
-  // This plugin is automatically enabled for projects which are GradleEnterprisePlugin.
+  // This plugin is automatically enabled for projects which have GradleEnterprisePlugin.
   override def trigger = allRequirements
 
-  override lazy val globalSettings: Seq[Setting[_]] = Seq(
-    helloGreeting := "hi from CCUD plugin"
-  )
-
   override lazy val projectSettings: Seq[Setting[_]] = Seq(
-    hello := {
-      val s = streams.value
-      val g = helloGreeting.value
-      s.log.info(g)
-    },
     GradleEnterprisePlugin.autoImport.gradleEnterpriseConfiguration := applyCCUD(
       GradleEnterprisePlugin.autoImport.gradleEnterpriseConfiguration.value,
       scalaVersion.value)

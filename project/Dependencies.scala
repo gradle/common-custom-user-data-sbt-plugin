df563c76f19635aa77f1dc2ead9ba6af4773b767
Gasper Kojek <gkojek@gradle.com>
2024-02-20 14:46:42 +0100

    Local development

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,5 +2,6 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.15"
+//  lazy val develocityPlugin = "com.gradle" % "sbt-develocity" % "0.11-for-local-testing"
   lazy val develocityPlugin = "com.gradle" % "sbt-develocity" % "1.0"
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,4 +1,5 @@
 resolvers += Resolver.mavenLocal
+//lazy val dvVersion = "0.11-for-local-testing"
 lazy val dvVersion = "1.0"
 addSbtPlugin("com.gradle" % "sbt-develocity" % dvVersion)
 //addSbtPlugin("com.gradle" % "sbt-common-custom-user-data" % "0.1.0-SNAPSHOT")

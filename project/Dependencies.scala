d5a45bd129de9fc8155d48e7342ae8fdbdf6db13
Iulian Dragos <idragos@gradle.com>
2025-02-28 10:13:59 +0100

    Rewrite to the new Develocity builder API
    Also upgraded the dependency to sbt-develocity, as the old methods were not deprecated before 1.1.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,5 +2,5 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.15"
-  lazy val develocityPlugin = "com.gradle" % "sbt-develocity" % "1.0"
+  lazy val develocityPlugin = "com.gradle" % "sbt-develocity" % "1.1.2"
 }

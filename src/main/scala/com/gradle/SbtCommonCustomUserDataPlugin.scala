f513c9b954523e953442ac4081c1355e926fa801
Martin Duhem <mduhem@gradle.com>
2025-09-12 11:51:35 +0200

    WIP - Apply CCUD after user load

--- src/main/scala/com/gradle/SbtCommonCustomUserDataPlugin.scala
+++ src/main/scala/com/gradle/SbtCommonCustomUserDataPlugin.scala
@@ -3,7 +3,7 @@ package com.gradle
 import com.gradle.develocity.agent.sbt.DevelocityPlugin
 import com.gradle.develocity.agent.sbt.DevelocityPlugin.autoImport.{DevelocityConfiguration, develocityConfiguration}
 import com.gradle.internal.{CustomBuildScanEnhancements, Overrides}
-import sbt.{AutoPlugin, Keys, Logger, Plugins, Setting}
+import sbt.{AttributeKey, AutoPlugin, BuiltinCommands, Keys, Logger, Plugins, Project, Setting, ThisBuild, sbtSlashSyntaxRichReference}
 
 object SbtCommonCustomUserDataPlugin extends AutoPlugin {
 
@@ -12,12 +12,23 @@ object SbtCommonCustomUserDataPlugin extends AutoPlugin {
   // This plugin is automatically enabled for projects which have DevelocityPlugin.
   override def trigger = allRequirements
 
-  override lazy val buildSettings: Seq[Setting[_]] = Seq(
-    develocityConfiguration := {
-      applyCCUD(
-        Keys.sLog.value,
-        develocityConfiguration.value
-      )
+  private val CcudApplied = AttributeKey[Unit]("ccud-applied", "Marker attribute to indicate that common custom user data has been applied")
+  override lazy val globalSettings: Seq[Setting[_]] = Seq(
+    Keys.onLoad := {
+      val original = Keys.onLoad.value
+      original andThen { state =>
+        state.get(CcudApplied) match {
+          case Some(_) =>
+            state // already applied
+          case None =>
+            val extracted = Project.extract(state)
+            val develocityConfigurationKey = ThisBuild / develocityConfiguration
+            val configuration = extracted.get(develocityConfigurationKey)
+            val newConfiguration = applyCCUD(state.log, configuration)
+            val newSettings = Project.setAll(extracted, Seq(ThisBuild / develocityConfigurationKey := newConfiguration))
+            BuiltinCommands.reapply(newSettings, extracted.structure, state.put(CcudApplied, ()))
+        }
+      }
     }
   )
 
